e401ca3e5717fef984724f8d71b0fa2cc9af47e7
Juanma López G <juanmanuel.lpzg@gmail.com>
2023-03-27 10:40:54 -0300

    Add pagination feature

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::net::SocketAddr;
 use axum::{
     Json,
     Router, routing::get};
+use axum::extract::Query;
 use serde::{Deserialize, Serialize};
 use tower_http::cors::{Any, CorsLayer};
 
@@ -31,12 +32,24 @@ struct Category {
     updated_at: String,
 }
 
-async fn get_products() -> Json<Vec<Product>> {
+#[derive(Debug, Deserialize, Serialize)]
+struct Params {
+    page: Option<i8>,
+    limit: Option<i8>,
+}
+
+async fn get_products(Query(params): Query<Params>) -> Json<Vec<Product>> {
     let url = "https://api.escuelajs.co/api/v1/products";
     let res = reqwest::get(url).await.expect("failed to get response");
     let products: Vec<Product> = res.json().await.expect("failed to parse response");
-    Json(products)
+
+    let start = (params.page.unwrap_or(1) - 1) * params.limit.unwrap_or(10);
+    let end = start + params.limit.unwrap_or(100);
+    let paginated_products = products.into_iter().skip(start as usize).take((end - start) as usize).collect();
+
+    Json(paginated_products)
 }
+
 
 #[tokio::main]
 async fn main() {
